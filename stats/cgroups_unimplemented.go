199114680d5ba6041d1722e72f080bfe39e2b242
Phil Estes <estesp@gmail.com>
2020-12-07 15:38:46 -0500

    Enable build on Windows
    Signed-off-by: Phil Estes <estesp@linux.vnet.ibm.com>

---
+++ stats/cgroups_unimplemented.go
@@ -0,0 +1,25 @@
+// +build windows
+
+package stats
+
+import (
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// CGroupsSampler is a stub on Windows
+type CGroupsSampler struct {
+	lastCPUUsage uint64
+	lastCPUTime  time.Time
+}
+
+// NewCGroupsSampler creates a stats sampler from existing control group
+func NewCGroupsSampler(path string) (*CGroupsSampler, error) {
+	return nil, errors.New("unimplemented")
+}
+
+// Query gets a process metrics from control cgroup
+func (s *CGroupsSampler) Query() (*ProcMetrics, error) {
+	return nil, errors.New("unimplemented")
+}
